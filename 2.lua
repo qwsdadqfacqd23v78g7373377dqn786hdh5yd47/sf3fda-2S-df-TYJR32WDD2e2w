fdade540455a6826257b624711e5a0c0943ae0b0
1p2o3l4k <dimassherliana@gmail.com>
2024-10-11 06:04:13 +0700

    Update and rename 1.lua to 2.lua

--- 2.lua
+++ 2.lua
@@ -94,7 +94,7 @@ copyDiscordButton.Parent = frame
 local animationText = Instance.new("TextLabel")
 animationText.Size = UDim2.new(1, 0, 1, 0)
 animationText.Position = UDim2.new(0, 0, 0, 0)
-animationText.Text = "Hello!"
+animationText.Text = "Hello :"..LocalPlayer.Name
 animationText.TextColor3 = Color3.fromRGB(255, 255, 255)
 animationText.BackgroundTransparency = 1
 animationText.Font = Enum.Font.GothamBold
@@ -102,6 +102,10 @@ animationText.TextSize = 40
 animationText.Parent = screenGui
 
 wait(1)
+animationText.Text = "Welcome Brothers"
+wait(3)
+animationText.Text = "Subscribe Medusa Script"
+wait(3)
 animationText.Text = "Thanks For Using"
 animationText.Visible = false
 frame.Visible = true
